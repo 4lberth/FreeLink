74d4290c334b3f1e273e44c8cccc78052f11b4ff
EstebanChavezV <esteban.chavez@tecsup.edu.pe>
2025-11-08 22:29:21 -0500

    feat(admin): implementar comando para cambiar el tipo de usuario

---
+++ FreeLink.Application/UseCase/Admin/Commands/ChangeUserType/ChangeUserTypeCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace FreeLink.Application.UseCase.Admin.Commands.ChangeUserType;
+
+// Devuelve 'bool' (verdadero) si tuvo éxito
+public class ChangeUserTypeCommand : IRequest<bool>
+{
+    public int UserId { get; set; }
+
+    // Usamos 'string' porque así lo definimos en tu User.cs
+    public string NewUserType { get; set; } 
+}
