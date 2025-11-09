348a15daa85b7cfc6bfc451f0e9f5a7e4d651a6a
EstebanChavezV <esteban.chavez@tecsup.edu.pe>
2025-11-08 22:34:33 -0500

    feat(admin): exponer endpoint para cambiar el tipo de usuario

--- FreeLink/Controllers/AdminController.cs
+++ FreeLink/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
-﻿using MediatR; 
+﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using FreeLink.Application.UseCase.Admin.Queries.GetAllUsers;
 using FreeLink.Application.UseCase.Admin.Commands.SetUserActiveStatus;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
+using FreeLink.Application.UseCase.Admin.Commands.ChangeUserType;
 
 namespace FreeLink.Controllers
 {
@@ -13,26 +15,23 @@ namespace FreeLink.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IMediator _mediator; 
-
-        public AdminController(IMediator mediator) 
+    
+        public AdminController(IMediator mediator)
         {
             _mediator = mediator;
         }
-
+    
         [HttpGet("users")]
         public async Task<IActionResult> GetAllUsers()
         {
-            // Crea y envía la "Query"
             var query = new GetAllUsersQuery();
             var users = await _mediator.Send(query);
             return Ok(users);
         }
 
-        // 👇 MÉTODO 2: AÑADE ESTE ENDPOINT PARA ACTIVAR/DESACTIVAR
         [HttpPost("users/set-active")]
         public async Task<IActionResult> SetUserActiveStatus([FromBody] SetUserActiveStatusCommand command)
         {
-            // Envía el "Command"
             var result = await _mediator.Send(command);
             
             if (!result)
@@ -40,6 +39,20 @@ namespace FreeLink.Controllers
                 return NotFound("No se pudo encontrar al usuario.");
             }
             
+            return Ok(new { success = true });
+        }
+
+        // 👇 AÑADE ESTE NUEVO ENDPOINT PARA CAMBIAR EL TIPO DE USUARIO
+        [HttpPost("users/change-type")]
+        public async Task<IActionResult> ChangeUserType([FromBody] ChangeUserTypeCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result)
+            {
+                return NotFound("No se pudo encontrar al usuario.");
+            }
+
             return Ok(new { success = true });
         }
     }
