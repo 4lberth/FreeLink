8777d7513b917a797230526c34c7e044cb665242
EstebanChavezV <esteban.chavez@tecsup.edu.pe>
2025-11-08 22:08:01 -0500

    feat(admin): implement query to get all users and cleanup .gitignore

--- FreeLink.Application/Mappings/MappingProfile.cs
+++ FreeLink.Application/Mappings/MappingProfile.cs
@@ -2,6 +2,7 @@
 using FreeLink.Application.UseCase.User.Commands.RegisterUser;
 using FreeLink.Application.UseCase.User.DTOs;
 using FreeLink.Domain.Entities;
+using FreeLink.Application.UseCase.Admin.Queries.GetAllUsers;
 
 namespace FreeLink.Application.Mappings;
 
@@ -11,5 +12,6 @@ public class MappingProfile : Profile
     {
         CreateMap<RegisterUserCommand, Userprofile>();
         CreateMap<User, UserDto>();
+        CreateMap<User, AdminUserDto>();
     }
 }
---
+++ FreeLink.Application/UseCase/Queries/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace FreeLink.Application.UseCase.Admin.Queries.GetAllUsers;
+
+public class GetAllUsersQuery : IRequest<IEnumerable<AdminUserDto>> 
+{ 
+    // Esta clase puede estar vacía
+}
