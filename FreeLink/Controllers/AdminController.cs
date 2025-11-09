7fb54bbc18ec9fb8c0aef61125e229b35e36982e
EstebanChavezV <esteban.chavez@tecsup.edu.pe>
2025-11-08 20:35:31 -0500

    feat(admin): setup base admin controller

---
+++ FreeLink/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR; 
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeLink.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")] 
+    public class AdminController : ControllerBase
+    {
+        private readonly IMediator _mediator; 
+
+        public AdminController(IMediator mediator) 
+        {
+            _mediator = mediator;
+        }
+
+        // Aquí pondremos los endpoints
+    }
+}
